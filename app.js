c43c13d6068342791a883e4b68261196c8a96bc7
khushikikani04 <152688579+khushikikani04@users.noreply.github.com>
2025-08-14 15:37:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ import { PORT } from './config/env.js';
 
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
-import subscriptionRouter from './routes/subscription.rotues.js';
+import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
-// import cookieParser from 'cookie-parser';
 
 const app = express();
 
