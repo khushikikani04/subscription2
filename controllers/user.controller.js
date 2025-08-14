f8f5eafb78e150fc9a04adc3e0a03ce7834ae9c2
khushikikani04 <152688579+khushikikani04@users.noreply.github.com>
2025-08-14 18:10:08 +0530

    Update user.controller.js

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,27 +1,41 @@
-import User from '../models/user.model.js'
+import User from '../models/user.model.js';
 
 export const getUsers = async (req, res, next) => {
   try {
     const users = await User.find();
-
     res.status(200).json({ success: true, data: users });
   } catch (error) {
     next(error);
   }
-}
+};
 
 export const getUser = async (req, res, next) => {
   try {
     const user = await User.findById(req.params.id).select('-password');
-
     if (!user) {
       const error = new Error('User not found');
       error.statusCode = 404;
       throw error;
     }
-
     res.status(200).json({ success: true, data: user });
   } catch (error) {
     next(error);
   }
-}
+};
+
+export const createUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // Optional: check if email exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ success: false, message: 'User already exists' });
+    }
+
+    const user = await User.create({ email, password });
+    res.status(201).json({ success: true, data: user });
+  } catch (error) {
+    next(error);
+  }
+};
