0d893ce54160d35837e35dfd07c148b7903f68b0
khushikikani04 <152688579+khushikikani04@users.noreply.github.com>
2025-08-15 15:57:11 +0530

    Update arcjet.js

--- config/arcjet.js
+++ config/arcjet.js
@@ -1,21 +1,39 @@
-import { tokenBucket } from '@arcjet/node'
-import {ARJECT_KEY} from './env.js' 
+// import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
+// import { ARCJET_KEY } from './env.js'
+
+// const aj = arcjet({
+//   key: ARCJET_KEY,
+//   characteristics: ["ip.src"],
+//   rules: [
+//     shield({ mode: "LIVE" }),
+//     detectBot({
+//       mode: "LIVE",
+//       allow: [ "CATEGORY:SEARCH_ENGINE" ],
+//     }),
+//     tokenBucket({
+//       mode: "LIVE",
+//       refillRate: 5, // Refill 5 tokens per interval
+//       interval: 10, // Refill every 10 seconds
+//       capacity: 10, // Bucket capacity of 10 tokens
+//     }),
+//   ],
+// });
+
+// export default aj;
+
+
+// config/arcjet.js
+import arcjet from "@arcjet/node";
 
 const aj = arcjet({
-  key: ARCJET_KEY,
-  Characteristics: ["ip.src"], 
-  rules: [
-    shield({ mode: "LIVE" }),
-    detectBot({
-      mode: "LIVE", 
-      allow: ["CATEGORY:SEARCH_ENGINE" ],
-    }),
-    // create a token bucket rate limit. other algorithma are supported.
-    tokenBucket({
-        mode: "LIVE",
-        refillRate: 5,  //Refill 5 tokens per interval
-        interval: 10,  //Refill every 10 seconds
-        capacity: 10,  //Bucket capacity of 10 tokens
-    }),
-  ],
-}),
+    key: process.env.ARCJET_KEY, // from .env
+    rules: [
+        {
+            type: "bot",
+            mode: "report" // 👈 only reports, doesn't block
+        }
+    ]
+});
+
+export default aj;
+
