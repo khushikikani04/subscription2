3692d689786bb74756342b96e07f8f670c52dc53
khushikikani04 <152688579+khushikikani04@users.noreply.github.com>
2025-08-15 10:05:02 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+
+import authorize from '../middlewares/auth.middleware.js';
+
 import { getUser, getUsers, createUser } from '../controllers/user.controller.js';
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
-userRouter.get('/:id', getUser);
+userRouter.get('/:id', authorize, getUser);
 userRouter.post('/', createUser);
 userRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE user' }));
 userRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE user' }));
