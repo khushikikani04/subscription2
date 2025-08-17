6925abd473bc21187c6adc7dd89102ece6cb519d
khushikikani04 <152688579+khushikikani04@users.noreply.github.com>
2025-08-17 10:44:10 +0530

    Update env.js

--- config/env.js
+++ config/env.js
@@ -1,12 +1,32 @@
 
+// import { config } from "dotenv";
+
+// config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
+
+// export const {
+//      PORT, NODE_ENV,
+//      DB_URI,
+//      JWT_SECRET, JWT_EXPIRES_IN,
+//      ARCJET_ENV, ARCJET_KEY,
+//      QSTASH_TOKEN, QSTASH_URL
+// } = process.env;
+
+
 import { config } from "dotenv";
 
+// Load env file according to NODE_ENV
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const {
-     PORT, NODE_ENV,
-     DB_URI,
-     JWT_SECRET, JWT_EXPIRES_IN,
-     ARCJET_ENV, ARCJET_KEY,
-     QSTASH_TOKEN, QSTASH_URL,
+  PORT,
+  NODE_ENV,
+  DB_URI,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
+  ARCJET_ENV,
+  ARCJET_KEY,
+  QSTASH_TOKEN,
+  QSTASH_URL,
+  SERVER_URL,
+  EMAIL_PASSWORD  
 } = process.env;
