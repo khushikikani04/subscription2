a377312906b287d0b2b05bd3d93183a233a0a982
khushikikani04 <152688579+khushikikani04@users.noreply.github.com>
2025-08-19 11:28:33 +0530

    Update subscription.controller.js

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,104 +1,74 @@
-// import Subscription from '../models/subscription.model.js'
-// import { workflowClient } from '../config/upstash.js'
-// // import { SERVER_URL } from '../config/env.js'
+// // controllers/subscription.controller.js
+// import Subscription from "../models/subscription.model.js";  
 
-// export const createSubscription = async (req, res, next) => {
+// // 🟢 Create new subscription
+// export const createSubscription = async (req, res) => {
 //   try {
-//     const subscription = await Subscription.create({
-//       ...req.body,
-//       user: req.user._id,
-//     });
-
-//     const { workflowRunId } = await workflowClient.trigger({
-//       url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
-//       body: {
-//         subscriptionId: subscription.id,
-//       },
-//       headers: {
-//         'content-type': 'application/json',
-//       },
-//       retries: 0,
-//     })
-
-//     res.status(201).json({ success: true, data: { subscription, workflowRunId } });
-//   } catch (e) {
-//     next(e);
+//     const subscription = await Subscription.create(req.body);
+//     res.status(201).json({ success: true, data: subscription });
+//   } catch (err) {
+//     res.status(400).json({ success: false, error: err.message });
 //   }
-// }
+// };
 
-// export const getUserSubscriptions = async (req, res, next) => {
+// // 🟢 Get all subscriptions for logged-in user
+// export const getUserSubscriptions = async (req, res) => {
 //   try {
-//     // Check if the user is the same as the one in the token
-//     if(req.user.id !== req.params.id) {
-//       const error = new Error('You are not the owner of this account');
-//       error.status = 401;
-//       throw error;
-//     }
+//     // मान लो req.user.id में user का ID stored है (JWT से)
+//     const subscriptions = await Subscription.find({ user: req.user.id });
 
-//     const subscriptions = await Subscription.find({ user: req.params.id });
-
-//     res.status(200).json({ success: true, data: subscriptions });
-//   } catch (e) {
-//     next(e);
-//   }
-// }
-
-
-// import Subscription from '../models/subscription.model.js'
-// import { workflowClient } from '../config/upstash.js'
-// import { SERVER_URL } from '../config/env.js'   // 👈 अब import हो गया
-
-// export const createSubscription = async (req, res, next) => {
-//   try {
-//     const subscription = await Subscription.create({
-//       ...req.body,
-//       user: req.user._id,
+//     res.status(200).json({
+//       success: true,
+//       count: subscriptions.length,
+//       data: subscriptions,
 //     });
-
-//     const { workflowRunId } = await workflowClient.trigger({
-//       url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
-//       body: {
-//         subscriptionId: subscription.id,
-//       },
-//       headers: {
-//         'content-type': 'application/json',
-//       },
-//       retries: 0,
-//     });
-
-//     res.status(201).json({ success: true, data: { subscription, workflowRunId } });
-//   } catch (e) {
-//     next(e);
+//   } catch (err) {
+//     res.status(500).json({ success: false, error: err.message });
 //   }
 // };
 
 
+import Subscription from '../models/subscription.model.js'
+import { workflowClient } from '../config/upstash.js'
+import { SERVER_URL } from '../config/env.js'
 
-// controllers/subscription.controller.js
-import Subscription from "../models/subscription.model.js";  
-
-// 🟢 Create new subscription
-export const createSubscription = async (req, res) => {
+export const createSubscription = async (req, res, next) => {
   try {
-    const subscription = await Subscription.create(req.body);
-    res.status(201).json({ success: true, data: subscription });
-  } catch (err) {
-    res.status(400).json({ success: false, error: err.message });
-  }
-};
-
-// 🟢 Get all subscriptions for logged-in user
-export const getUserSubscriptions = async (req, res) => {
-  try {
-    // मान लो req.user.id में user का ID stored है (JWT से)
-    const subscriptions = await Subscription.find({ user: req.user.id });
-
-    res.status(200).json({
-      success: true,
-      count: subscriptions.length,
-      data: subscriptions,
+    const subscription = await Subscription.create({
+      ...req.body,
+      user: req.user._id,
     });
-  } catch (err) {
-    res.status(500).json({ success: false, error: err.message });
+
+    const { workflowRunId } = await workflowClient.trigger({
+      url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+      body: {
+        subscriptionId: subscription.id,
+      },
+      headers: {
+        'content-type': 'application/json',
+      },
+      retries: 0,
+    })
+
+    res.status(201).json({ success: true, data: { subscription, workflowRunId } });
+  } catch (e) {
+    next(e);
+  }
+}
+
+export const getUserSubscriptions = async (req, res, next) => {
+  try {
+    // Check if the user is the same as the one in the token
+    if(req.user.id !== req.params.id) {
+      const error = new Error('You are not the owner of this account');
+      error.status = 401;
+      throw error;
+    }
+
+    const subscriptions = await Subscription.find({ user: req.params.id });
+
+    res.status(200).json({ success: true, data: subscriptions });
+  } catch (e) {
+    next(e);
   }
 };
