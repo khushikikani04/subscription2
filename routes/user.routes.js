2f837294f4729c1c67f7789ff0bb2e0906e80248
khushikikani04 <152688579+khushikikani04@users.noreply.github.com>
2025-08-19 09:39:03 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,15 +1,38 @@
+// import { Router } from 'express';
+
+// import authorize from '../middlewares/auth.middleware.js';
+
+// import { getUser, getUsers, createUser } from '../controllers/user.controller.js';
+
+// const userRouter = Router();
+
+// userRouter.get('/', getUsers);
+// userRouter.get('/:id', authorize, getUser);
+// userRouter.post('/', createUser);
+// userRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE user' }));
+// userRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE user' }));
+
+// export default userRouter;
+
+
+
 import { Router } from 'express';
 
 import authorize from '../middlewares/auth.middleware.js';
 
 import { getUser, getUsers, createUser } from '../controllers/user.controller.js';
+import { signIn } from '../controllers/auth.controller.js'; // new controller for login
 
 const userRouter = Router();
 
+// User CRUD
 userRouter.get('/', getUsers);
 userRouter.get('/:id', authorize, getUser);
 userRouter.post('/', createUser);
 userRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE user' }));
 userRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE user' }));
+
+// 🔑 User login (sign-in)
+userRouter.post('/sign-in', signIn);
 
 export default userRouter;
