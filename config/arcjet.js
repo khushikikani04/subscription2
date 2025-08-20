52166eec589fa6b4f161431cbc6c973c9f1c1458
khushikikani04 <152688579+khushikikani04@users.noreply.github.com>
2025-08-20 18:56:08 +0530

    Update arcjet.js

--- config/arcjet.js
+++ config/arcjet.js
@@ -22,45 +22,39 @@
 // export default aj;
 
 
-// // config/arcjet.js
-// import arcjet from "@arcjet/node";
 
-// const aj = arcjet({
-//     key: process.env.ARCJET_KEY, // from .env
-//     rules: [
-//         {
-//             type: "bot",
-//             mode: "report" // 👈 only reports, doesn't block
-//         }
-//     ]
-// });
-
-// export default aj;
-
-
-
-// import arcjet, { protect } from "@arcjet/node";
+// import arcjet, { detectBot } from "@arcjet/node";
 
 // const aj = arcjet({
 //   key: process.env.ARCJET_KEY,
 //   rules: [
-//     protect.Bot({
-//       // mode: "DRY_RUN",  // या "LIVE"
-//       allow: ["CATEGORY:SEARCH_ENGINE"], 
+//     detectBot({
+//       mode: "DRY_RUN",  // or "DRY_RUN" if you're still testing
+//       allow: ["CATEGORY:SEARCH_ENGINE"],
 //     }),
 //   ],
 // });
 
 // export default aj;
 
-import arcjet, { detectBot } from "@arcjet/node";
+
+import arcjet, { shield, detectBot, tokenBucket } from "@arcjet/node";
+import { ARCJET_KEY } from './env.js'
 
 const aj = arcjet({
-  key: process.env.ARCJET_KEY,
+  key: ARCJET_KEY,
+  characteristics: ["ip.src"],
   rules: [
+    shield({ mode: "LIVE" }),
     detectBot({
-      mode: "DRY_RUN",  // or "DRY_RUN" if you're still testing
-      allow: ["CATEGORY:SEARCH_ENGINE"],
+      mode: "LIVE",
+      allow: [ "CATEGORY:SEARCH_ENGINE" ],
+    }),
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 5, // Refill 5 tokens per interval
+      interval: 10, // Refill every 10 seconds
+      capacity: 10, // Bucket capacity of 10 tokens
     }),
   ],
 });
