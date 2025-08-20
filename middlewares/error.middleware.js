01c5e88a3a538de0d25aadf1ddd28f75cd709165
khushikikani04 <152688579+khushikikani04@users.noreply.github.com>
2025-08-20 17:24:45 +0530

    Update error.middleware.js

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -6,21 +6,18 @@ const errorMiddleware = (err, req, res, next) => {
 
         console.error(err);
 
-        //Mongoose bad objectId
         if (err.name === 'CassrError'){
             const message = 'Resource not found';
             error = new Error(message);
             error.statuscode = 404;
        }
 
-        //Mongoose duplicate key
         if (err.code === 11000) {
             const message = 'Duplicate field value entered';
             error = new Error(message);
             error.statuscode = 400;
        }
 
-        //Mongoose validation error
         if (err.name === 'validationError') {
             const message = Object.values(err.erros).map(val => val.message);
             error = new Error(message.join(', '));
