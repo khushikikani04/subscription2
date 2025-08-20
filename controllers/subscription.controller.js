874d48e65113afd6f9fbf9e8c20769eeb3829bea
khushikikani04 <152688579+khushikikani04@users.noreply.github.com>
2025-08-20 18:56:03 +0530

    Update subscription.controller.js

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -28,6 +28,52 @@
 // };
 
 
+// import Subscription from '../models/subscription.model.js'
+// import { workflowClient } from '../config/upstash.js'
+// import { SERVER_URL } from '../config/env.js'
+
+// export const createSubscription = async (req, res, next) => {
+//   try {
+//     const subscription = await Subscription.create({
+//       ...req.body,
+//       user: req.user._id,
+//     });
+
+//     const { workflowRunId } = await workflowClient.trigger({
+//       url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+//       body: {
+//         subscriptionId: subscription.id,
+//       },
+//       headers: {
+//         'content-type': 'application/json',
+//       },
+//       retries: 0,
+//     })
+
+//     res.status(201).json({ success: true, data: { subscription, workflowRunId } });
+//   } catch (e) {
+//     next(e);
+//   }
+// }
+
+// export const getUserSubscriptions = async (req, res, next) => {
+//   try {
+//     if(req.user.id !== req.params.id) {
+//       const error = new Error('You are not the owner of this account');
+//       error.status = 401;
+//       throw error;
+//     }
+
+//     const subscriptions = await Subscription.find({ user: req.params.id });
+
+//     res.status(200).json({ success: true, data: subscriptions });
+//   } catch (e) {
+//     next(e);
+//   }
+// };
+
+
+
 import Subscription from '../models/subscription.model.js'
 import { workflowClient } from '../config/upstash.js'
 import { SERVER_URL } from '../config/env.js'
@@ -50,6 +96,7 @@ export const createSubscription = async (req, res, next) => {
       retries: 0,
     })
 
+    console.log("done calling trigger")
     res.status(201).json({ success: true, data: { subscription, workflowRunId } });
   } catch (e) {
     next(e);
@@ -58,7 +105,6 @@ export const createSubscription = async (req, res, next) => {
 
 export const getUserSubscriptions = async (req, res, next) => {
   try {
-    // Check if the user is the same as the one in the token
     if(req.user.id !== req.params.id) {
       const error = new Error('You are not the owner of this account');
       error.status = 401;
@@ -71,4 +117,4 @@ export const getUserSubscriptions = async (req, res, next) => {
   } catch (e) {
     next(e);
   }
-};
+}
